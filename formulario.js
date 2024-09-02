4a047ccecd96a2fa0d8be2a264c3975eab51f645
erickmth <160558291+erickmth@users.noreply.github.com>
2024-09-02 19:59:34 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,31 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const applyButton = document.getElementById('apply-btn');
+    const formPopup = document.getElementById('form-popup');
+    const closeButton = document.getElementById('close-btn');
+    const form = document.getElementById('application-form');
 
     applyButton.addEventListener('click', () => {
-        const name = prompt('Nome completo:');
-        const birthDate = prompt('Data de Nascimento (dd/mm/aaaa):');
-        const phone = prompt('Telefone para contato:');
+        formPopup.style.display = 'flex'; // Exibe o formulário
+    });
+
+    closeButton.addEventListener('click', () => {
+        formPopup.style.display = 'none'; // Oculta o formulário
+    });
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault(); // Previne o comportamento padrão do formulário
+
+        const name = document.getElementById('name').value;
+        const birthDate = document.getElementById('birthDate').value;
+        const phone = document.getElementById('phone').value;
 
         if (name && birthDate && phone) {
             const emailBody = `Olá, me chamo ${name}, tenho ${calculateAge(birthDate)} anos e estou interessado na vaga Bolos da Vovó! Meu telefone de contato: ${phone}`;
-            const mailtoLink = `mailto:atendimentobolosdavovo@gmail.com?subject=Candidatura para a Vaga&body=${encodeURIComponent(emailBody)}`;
-
-            window.open(mailtoLink, '_blank');
-        } else {
-            alert('Por favor, preencha todos os campos.');
-        }
-    });
-
-    function calculateAge(birthDate) {
-        const today = new Date();
-        const birth = new Date(birthDate.split('/').reverse().join('-'));
-        let age = today.getFullYear() - birth.getFullYear();
-        const monthDifference = today.getMonth() - birth.getMonth();
-
-        if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birth.getDate())) {
-            age--;
-        }
-
-        return age;
-    }
-});
+            const mailtoLink = `mailto:atendimentobolosdav
